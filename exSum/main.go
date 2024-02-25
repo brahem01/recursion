99e65c41df714339dee80b66d7e71e58507019e9
brahem <moroccobrahem@gmail.com>
2024-02-25 11:08:00 +0000

    exSum

--- exSum/main.go
+++ exSum/main.go
@@ -3,17 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var n uint64 = 4
-	fmt.Println(expSum(n))
+	fmt.Println(expSum(uint64(10)))
 }
 
 func expSum(n uint64) uint64 {
 	partitions := make([]uint64, n+1)
 	partitions[0] = 1
-	var i uint64 = 1
-	for ; i <= n; i++ {
-		j := i
-		for ; j <= n; j++ {
+	for i := uint64(1); i <= n; i++ {
+		for j := i; j <= n; j++ {
 			partitions[j] += partitions[j-i]
 		}
 	}
